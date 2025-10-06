04ab030e8427abe5cf740ab8b13b21fb9a578001
Kevin Pennock <kpennock@usf.edu>
2025-10-05 21:58:41 -0400

    step 6

--- cc_6.js
+++ cc_6.js
@@ -1,40 +1,51 @@
 class Employee {
-    constructor (name,department,){
-        this.name = name;
-        this.department = department;
-    }
-    describe(){
-        return `${this.name}works in this${this.department}`;
-    }
+  constructor(name, department) {
+    this.name = name;
+    this.department = department;
+  }
+
+  describe() {
+    return `${this.name} works in the ${this.department} department.`;
+  }
 }
-class Manager extends Employee{
-    constructor(name,department,teamSize){
-        super(name,department,teamSize)
-        this.name = name
-        this.department = department
-        this.teamSize = teamSize 
-    }
-    describe(){
-        return `${this.name}works in this${this.department}with this team size${this.teamSize}`
-    }
+
+class Manager extends Employee {
+  constructor(name, department, teamSize) {
+    super(name, department);
+    this.teamSize = teamSize;
+  }
+
+  describe() {
+    return `${this.name} manages the ${this.department} department with a team of ${this.teamSize}.`;
+  }
 }
+
 let newEmployees = [
-    new Employee("Isabella", "Sales"),
-    new Employee("Juan", "Production"),
+  new Employee("Isabella", "Sales"),
+  new Employee("Juan", "Production"),
 ];
-let emp = new Employee(Maria, Design)
-let man1 = new Manager(Sofia, Design)
+
+let emp = new Employee("Maria", "Design");
+let man1 = new Manager("Sofia", "Design", 5);
 
 class Company {
-    constructor(Employees) {
-        this.Employees = Employees
-    }
-    addEmployee(employee) {
-    this.employee.push(employee);
+  constructor() {
+    this.employees = [];
   }
+
+  addEmployee(employee) {
+    this.employees.push(employee);
+  }
+
   listEmployees() {
     this.employees.forEach((employee) => {
-      console.log(`List: ${employee}`);
+      console.log(employee.describe());
     });
   }
 }
+let myCompany = new Company();
+myCompany.addEmployee(emp);
+myCompany.addEmployee(man1);
+newEmployees.forEach(e => myCompany.addEmployee(e));
+
+myCompany.listEmployees();
